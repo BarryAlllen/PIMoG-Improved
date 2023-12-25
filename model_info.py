eb44914217e2c8370e71439df2d99c38a700165b
Barry <1249683134@qq.com>
2023-12-25 16:27:33 +0800

    3.0 15bit model256

--- model_info.py
+++ model_info.py
@@ -1,14 +1,18 @@
-from model import Discriminator
-from model import Encoder_Decoder
-from model import U_Net_Encoder_Diffusion
+from model256 import Discriminator
+from model256 import Encoder_Decoder
+from model256 import U_Net_Encoder_Diffusion
 from torchinfo import summary
+import torch
+
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 encoder_input_size = [(1, 3, 128, 128), (1, 15)]
 
 def encoder_info(input_size):
-    encoder = U_Net_Encoder_Diffusion()
+    encoder = U_Net_Encoder_Diffusion().to(device)
     print(encoder)
-    summary(encoder, input_size=encoder_input_size)
+    summary(encoder, input_size=input_size)
+
 
 def main():
     encoder_info(encoder_input_size)
